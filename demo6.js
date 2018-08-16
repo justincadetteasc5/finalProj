2bee60eaac01e97cfab995135c317e7d72dc3884
justincadette <justincadette0@gmail.com>
2018-08-16 13:53:45 -0400

    push

---
+++ demo6.js
@@ -0,0 +1,48 @@
+div16 {
+    display: inline-block;
+    padding: 20px;
+}
+
+div17 {
+    display: inline-block;
+    padding: 20px;
+}
+
+div18 {
+    display: inline-block;
+    padding:20px;
+}
+
+div19 {
+    display: inline-block;
+    padding:20px;
+}
+
+div20 {
+    display:inline;
+    padding: 50px;
+    font-size: 30px;
+    text-decoration:underline;
+    font-weight:bold;
+}
+
+    <div16>
+            <a href ="https://www.binghamton.edu/financial-aid/">Binghamton College</a>
+            <img src = "https://www.logolynx.com/images/logolynx/47/4725dbbacb0e759a6736835c9963d2f0.gif"height="50px"width="50px"
+    </div16>
+    <div17>
+            <a href="https://barnard.edu/finaid">Barnard College</a>
+            <img src = "https://www.commonapp.org/files/styles/400-y/public/school/logo/Barnard%20logo-500x500.png?itok=j-HGbPyf"height="50px"width="50px"
+    </div17>
+    <div18>
+            <a href ="http://financialaid.buffalo.edu/">University at Buffalo</a>
+            <img src ="http://www.buffalo.edu/brand/resources-tools/downloads/_jcr_content/image.img.512.auto.jpg/1523904417385.jpg"height="50px"width="50px"
+    </div18>
+    <div19>
+            <a href ="http://www.colgate.edu/admission-financial-aid/financial-aid">Colgate University</a>
+            <img src ="http://www.colgate.edu/public/img/logo.png"height="50px"width="50px"
+    </div19>
+    <div20>
+        LITERATURE
+    </div20>
+</body>
